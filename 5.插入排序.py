1cfef70bc1413ae29ae571d923be2dc33cfc8b5c
lp1506947671 <1506946771@qq.com>
2021-06-15 21:30:50 +0800

    [类型]:需求开发 [描述]:插入排序 [提交人]:Jason

--- 5.插入排序.py
+++ 5.插入排序.py
@@ -2,3 +2,17 @@
 # -*- coding: utf-8 -*-
 
 
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+if __name__ == "__main__":
+    arr1 = [64, 34, 25, 12, 22, 11, 90]
+    insert_sort(arr1)
+    print(f"排序后的数组:{arr1}")
